4b0116486f600f9d71105a63cb11f4d734121f05
ssnoodles <ssnoodles0226@gmail.com>
2023-12-16 22:19:01 +0800

    :art: improve role add and delete

--- controllers/role.go
+++ controllers/role.go
@@ -5,6 +5,7 @@ import (
 	"gin-admin-template/models"
 	"gin-admin-template/services"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 	"strconv"
 )
@@ -12,6 +13,11 @@ import (
 type RoleQuery struct {
 	services.PageInfo
 	Name string `form:"name"`
+}
+
+type RoleAdd struct {
+	models.Role
+	MenuIds []int64 `json:"menuIds"`
 }
 
 func GetRoles(c *gin.Context) {
@@ -38,31 +44,95 @@ func GetRole(c *gin.Context) {
 }
 
 func CreateRole(c *gin.Context) {
-	var role models.Role
-	err := c.ShouldBindJSON(&role)
+	var roleAdd RoleAdd
+	err := c.ShouldBindJSON(&roleAdd)
 	if err != nil {
 		c.String(http.StatusBadRequest, "参数错误")
 		return
 	}
-	role.Id = initializations.IdGenerate()
-	err = services.Insert(&role)
+	roleId := initializations.IdGenerate()
+	err = initializations.DB.Transaction(func(tx *gorm.DB) error {
+		role := models.Role{
+			Id:    roleId,
+			Name:  roleAdd.Name,
+			Code:  roleAdd.Code,
+			OrgId: roleAdd.OrgId,
+		}
+		if err = tx.Create(&role).Error; err != nil {
+			return err
+		}
+		var rmr []models.RoleMenuRelation
+		for _, id := range roleAdd.MenuIds {
+			rmr = append(rmr, models.RoleMenuRelation{
+				Id:     initializations.IdGenerate(),
+				RoleId: roleId,
+				MenuId: id,
+			})
+		}
+		if err = initializations.DB.Create(&rmr).Error; err != nil {
+			return err
+		}
+		return nil
+	})
 	if err != nil {
-		c.String(http.StatusBadRequest, "参数错误")
+		c.String(http.StatusBadRequest, "新增失败")
 		return
 	}
-	c.JSON(http.StatusOK, models.NewIdWrapper(role.Id))
+	c.JSON(http.StatusOK, models.NewIdWrapper(roleId))
 }
 
 func UpdateRole(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
-	var role models.Role
-	err = c.ShouldBindJSON(&role)
+	var roleAdd RoleAdd
+	err = c.ShouldBindJSON(&roleAdd)
 	if err != nil {
 		c.String(http.StatusBadRequest, "参数错误")
 		return
 	}
-	role.Id = int64(id)
-	err = services.Update(&role)
+	roleId := int64(id)
+	var role models.Role
+	err = services.FindById(&role, roleId)
+	if err != nil {
+		c.String(http.StatusBadRequest, "角色不存在")
+		return
+	}
+	if roleAdd.Code != role.Code {
+		var role models.Role
+		err = services.FindByCode(&role, roleAdd.Name)
+		if err == nil {
+			c.String(http.StatusBadRequest, "名称已存在")
+			return
+		}
+	}
+	err = initializations.DB.Transaction(func(tx *gorm.DB) error {
+		role.Name = roleAdd.Name
+		role.Code = roleAdd.Code
+		role.OrgId = roleAdd.OrgId
+		if err = tx.Save(&role).Error; err != nil {
+			return err
+		}
+		var oldRmr []models.RoleMenuRelation
+		if err = tx.Where("role_id = ?", roleId).Find(&oldRmr).Error; err != nil {
+			return err
+		}
+		if len(oldRmr) > 0 {
+			if err = tx.Delete(oldRmr).Error; err != nil {
+				return err
+			}
+		}
+		var rmr []models.RoleMenuRelation
+		for _, id := range roleAdd.MenuIds {
+			rmr = append(rmr, models.RoleMenuRelation{
+				Id:     initializations.IdGenerate(),
+				RoleId: roleId,
+				MenuId: id,
+			})
+		}
+		if err = initializations.DB.Create(&rmr).Error; err != nil {
+			return err
+		}
+		return nil
+	})
 	if err != nil {
 		c.String(http.StatusBadRequest, "更新失败")
 		return
@@ -72,7 +142,21 @@ func UpdateRole(c *gin.Context) {
 
 func DeleteRole(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
-	err = services.DeleteById(models.Role{}, int64(id))
+	var role models.Role
+	err = services.FindById(&role, int64(id))
+	if err != nil {
+		c.String(http.StatusBadRequest, "数据不存在")
+		return
+	}
+	err = initializations.DB.Transaction(func(tx *gorm.DB) error {
+		if err = tx.Delete(&role).Error; err != nil {
+			return err
+		}
+		if err = tx.Where("role_id = ?", id).Delete(&models.RoleMenuRelation{}).Error; err != nil {
+			return err
+		}
+		return nil
+	})
 	if err != nil {
 		c.String(http.StatusBadRequest, "删除失败")
 		return
--- models/role.go
+++ models/role.go
@@ -6,6 +6,7 @@ type Role struct {
 	Id        int64     `json:"id" gorm:"primaryKey"`
 	Name      string    `json:"name" gorm:"not null;size:50"`
 	Code      string    `json:"code" gorm:"not null;size:50"`
+	OrgId     int64     `json:"orgId" gorm:"not null"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
